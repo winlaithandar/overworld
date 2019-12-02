b2bc54e2980ba62227fcd30893f56cd7782a38cb
danielgrijalva <jdgs.gt@gmail.com>
2019-12-02 12:24:17 -0700

    :sparkles: cover component now render optional activity icons

--- frontend/src/modules/app/components/cover/index.js
+++ frontend/src/modules/app/components/cover/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import { CoverWrapper, ImageLink, StyledCoverImage } from "./styles";
 
-const Cover = ({ imageId, slug, size }) => {
+const Cover = ({ imageId, slug, size, activity }) => {
   const coverUrl = `https://images.igdb.com/igdb/image/upload/t_cover_big/${imageId}.jpg`;
   return (
     <CoverWrapper size={size}>
       <ImageLink to={`/games/${slug}`}>
         <StyledCoverImage src={coverUrl} rounded fluid />
       </ImageLink>
+      {/* Render the activity icons in case there's any activity 
+      for each game. Note: this is used only in the profile, in the 
+      Recent Activity section */}
+      {activity}
     </CoverWrapper>
   );
 };
